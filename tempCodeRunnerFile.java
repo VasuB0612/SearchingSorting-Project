3280bdc8638cae8126f3a22e7a8af1179d631a0a
Vasu Bhatnagar <vasu.bhatnagar@tuni.fi>
2023-12-08 15:33:10 +0200

    Sorting performance TIER 5.

--- tempCodeRunnerFile.java
+++
@@ -1,7 +0,0 @@
-risonCounter.resetComparisonCount();
-            // long startTime = System.currentTimeMillis();
-            // insertionSort(arr);
-            // long endTime = System.currentTimeMillis();
-
-            // long elapsedTime = endTime - startTime;
-            // Syste
